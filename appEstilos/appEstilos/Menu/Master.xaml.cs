1fa8b80142974b56a19a07c770bf5b7d08ea90dd
Rafael Batista <maranata_rafa@hotmail.com>
2020-08-22 23:29:25 -0400

    Projeto concluído.

---
+++ appEstilos/appEstilos/Menu/Master.xaml.cs
@@ -0,0 +1,51 @@
+﻿using appEstilos.Pages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace appEstilos.Menu
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Master : MasterDetailPage
+    {
+        public Master()
+        {
+            InitializeComponent();
+        }
+
+        private void IrPaginaImplicitStylePage(object s, EventArgs e)
+        {
+            Detail = new NavigationPage(new ImplicitStylePage());
+            IsPresented = false;
+        }
+
+        private void IrPaginaExplicitStylePage(object s, EventArgs e)
+        {
+            Detail = new NavigationPage(new ExplicitStylePage());
+            IsPresented = false;
+        }
+
+        private void IrPaginaGlobalStylePage(object s, EventArgs e)
+        {
+            Detail = new NavigationPage(new GlobalStylePage());
+            IsPresented = false;
+        }
+
+        private void IrPaginaInheritStylePage(object s, EventArgs e)
+        {
+            Detail = new NavigationPage(new InheritStylePage());
+            IsPresented = false;
+        }
+
+        private void IrPaginaDynamicStylePage(object s, EventArgs e)
+        {
+            Detail = new NavigationPage(new DynamicStylePage());
+            IsPresented = false;
+        }
+    }
+}
---
+++ appEstilos/appEstilos/Pages/DynamicStylePage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace appEstilos.Pages
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class DynamicStylePage : ContentPage
+    {
+        public DynamicStylePage()
+        {
+            InitializeComponent();
+        }
+
+        private void MudarCor(object s, EventArgs e)
+        {
+            //this.Resources["lblColor"] = Color.Tomato;
+            this.Resources["lblColor"] = this.Resources["lbl"];
+        }
+    }
+}
